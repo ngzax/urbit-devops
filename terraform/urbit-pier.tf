f48ad427c2f399d77ad17393f274309e6e741ae2
Daryl Richter <daryl.richter@nrg.com>
2017-08-22 10:40:53 -0400

    D'Oh! Add the outbound routes

--- terraform/urbit-pier.tf
+++ terraform/urbit-pier.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "urbit-sg" {
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "urbit-pier" {
