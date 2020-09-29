8ec5d7a237641a39532aebbe97c94ccd01864b63
quaternioneer <nat.admin@protonmail.com>
2020-09-29 15:34:00 -0700

    Removed commented out code in all files except create-urbit-comet.exp.

--- terraform/provision-urbit.sh
+++ terraform/provision-urbit.sh
@@ -2,7 +2,6 @@
 sudo apt update
 
 # Install all urbit dependencies
-#sudo apt install -y epel gcc gcc-c++ gmp-devel openssl-devel ncurses-devel libsigsegv-devel ctags automake autoconf libtool cmake re2c libcurl-devel
 sudo apt install -y screen automake autoconf libtool cmake
 
 # Download build, and install the urbit source package
@@ -13,8 +12,6 @@ wget https://bootstrap.urbit.org/urbit-v0.10.8-linux64.tgz
 
 tar xfvz urbit-v0.10.8-linux64.tgz
 cd urbit-v0.10.8-linux64
-#make
-#sudo install -m 0755 urbit /usr/local/bin
 mv urbit* ../../piers
 
 # Add 2GB of swap so that we have enough memory to start up
